c630c0d76446db0a9e9ff0cee396de4f3029629e
G4GIFTEDHANDS <113684052+G4GIFTEDHANDS@users.noreply.github.com>
2023-04-30 21:34:46 +0100

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL passed as an argument, and displays only the status code of the response. no pipe
-awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
+# Sends a JSON POST request to a given URL with a given JSON file.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
